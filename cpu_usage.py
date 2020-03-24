385c1ba72e3f7b88e786ca3b8c78770bc1101f06
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-23 22:28:59 -0700

    git

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import psutil
 
+
 def check_cpu_usage(percent):
-    usage = psutil.cpu_percent()
+    # patch command was used to fix the file using diff file
+    usage = psutil.cpu_percent(1)
+    print("DEBUG: usage: {}".format(usage))
     return usage < percent
 
 if not check_cpu_usage(75):
     print("ERROR! CPU is overloaded")
 else:
     print("Everyting ok")
-
-
-
