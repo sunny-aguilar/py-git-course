9e9446b398b0a0816322445620e7dbd51d16f326
sunny-aguilar <sunny.aguilar@gmail.com>
2020-03-26 00:09:59 -0700

    git

--- all_checks.py
+++ all_checks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def check_reboot():
-    """"returns True if the computer has a pending reboot"""
+    """returns True if the computer has a pending reboot"""
 
 def main():
     pass
