5059e65356e52c29630ed22d07517c9d5bdbd83b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-03-26 22:20:19 -0700

    git.course

--- all_checks.py
+++ all_checks.py
@@ -10,6 +10,9 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    if disk_full():
+        print("Disk full.")
+        sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
 
