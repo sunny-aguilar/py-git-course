21ad8fe18701967cc2fcd9b89be6b6663dd08353
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-04 23:15:12 -0700

    git.course

--- usage.py
+++ usage.py
@@ -10,26 +10,26 @@ def bang(n):
 # return n to the power of k
 def pow(n, k):
     if k is 0:
-        return 1;
-    return n * pow(n, k-1);
+        return 1
+    return n * pow(n, k-1)
 
 
-n = 35;
-k = 0;
-result = 0;
+n = 35
+k = 0
+result = 0
 
 # calculate probability that ten or fewer are active at the same time
 # for (; k < 11; k++) {
 for i in range(11):
     # probability that exactly k users are active
-    probabilityOfSet = pow(0.1, k) * pow(0.9, n - k);
-    numberOfSets = bang(35)/(bang(35 - k)*bang(k)); # n choose k
-    result += probabilityOfSet * numberOfSets;
+    probabilityOfSet = pow(0.1, k) * pow(0.9, n - k)
+    numberOfSets = bang(35)/(bang(35 - k) * bang(k)) # n choose k
+    result += probabilityOfSet * numberOfSets
 
 
 # modify so result is the probability that 11 or more people are
 # active at the same time
-result = 1 - result;
+result = 1 - result
 
 # display result
 print(result)
