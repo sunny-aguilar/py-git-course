4e44843fdb2d625b714dd78d9440d4b034477a07
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-07 10:46:11 -0700

    git.course

---
+++ scripts/dailysync.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import suprocess
+import os
+
+src = "/data/prod/"
+dest = "/data/prod/backup/"
+
+for root, dirs, files in os.walk(src):
+    print(files)
+
+subprocess.call(["rsync", "-arq", src, dest])
+
+
+# dir path: student/data/prod/alpha beta gamma/files
+# dir path: student/data/prod_backup/empty
+# dir path: student/scripts
+
