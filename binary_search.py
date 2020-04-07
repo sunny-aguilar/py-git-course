4f0ca46af6921e8f550c3cc5b378450e571fcaf6
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-06 20:25:34 -0700

    git.course

--- binary_search.py
+++ binary_search.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 def find_item(list, item):
-  #Returns True if the item is in the list, False if not.
-  if len(list) == 0:
+    #Returns True if the item is in the list, False if not.
+    if len(list) == 0:
+        return False
+
+    #Is the item in the center of the list?
+    middle = len(list)//2
+    if list[middle] == item:
+        return True
+    print("middle: ", list[middle])  
+    #Is the item in the first half of the list? 
+    if item < list[middle]:
+        #Call the function with the first half of the list
+        return find_item(list[:middle], item)
+    else:
+        #Call the function with the second half of the list
+        return find_item(list[middle+1:], item)
+
     return False
-
-  #Is the item in the center of the list?
-  middle = len(list)//2
-  if list[middle] == item:
-    return True
-
-  #Is the item in the first half of the list? 
-  if item < list[middle]:
-    #Call the function with the first half of the list
-    return find_item(list[:middle], item)
-  else:
-    #Call the function with the second half of the list
-    return find_item(list[middle+1:], item)
-
-  return False
 
 #Do not edit below this line - This code helps check your work!
 list_of_names = ["Parker", "Drew", "Cameron", "Logan", "Alex", "Chris", "Terry", "Jamie", "Jordan", "Taylor"]
