9518ec6042d9fdf510384f951546d9039781db56
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-08 16:41:58 -0700

    git.course

--- start_date_report.py
+++ start_date_report.py
@@ -5,8 +5,10 @@ import requests
 
 FILE_URL="http://marga.com/ar/employees-with-date.csv"
 
+def get_start_date():
+    """Interactively get the start date to query for."""
 
-
-
+    print()
+    print('Getting the first start date to query for.')
 
 
